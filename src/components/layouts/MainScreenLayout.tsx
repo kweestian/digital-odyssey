394f4ccf66b04e5fac4d7b21fee9b31cf003917b
Pmmarteau <pm.marteau@gmail.com>
2023-04-04 14:58:05 +0200

    Remove unused import

--- src/components/layouts/MainScreenLayout.tsx
+++ src/components/layouts/MainScreenLayout.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import Image from 'next/image';
 
 import styles from './MainScreenLayout.module.scss';
-
-import * as Border from '../../../public/static/image/mainScreenBorder.svg';
 
 const MainScreen = ({ children, title }: {children: React.ReactNode, title: string}) => (
   <div className={styles.subLayout}>

1a8695ae62e396f4fb249e231e53bb21f11edc18
Pmmarteau <pm.marteau@gmail.com>
2023-04-04 14:57:26 +0200

    Refactoring main screen layout

--- src/components/layouts/MainScreenLayout.tsx
+++ src/components/layouts/MainScreenLayout.tsx
@@ -6,16 +6,13 @@ import styles from './MainScreenLayout.module.scss';
 import * as Border from '../../../public/static/image/mainScreenBorder.svg';
 
 const MainScreen = ({ children, title }: {children: React.ReactNode, title: string}) => (
-  <div className={styles.subLayoutContainer}>
-    <div className={styles.mainScreenBorder}>
-      <div className={styles.buttonContainer}>
-        <div className={styles.button}>
-          <h1>{title}</h1>
-        </div>
-      </div>
-      <Image src={Border} alt="" />
+  <div className={styles.subLayout}>
+    <div className={styles.buttonContainer}>
+      <h1>{title}</h1>
     </div>
-    {children}
+    <div className={styles.subLayoutContainer}>
+      {children}
+    </div>
   </div>
 );
 

b1977abf43279d6c6c10435cb9fa5ebe198bbf99
Pmmarteau <pm.marteau@gmail.com>
2023-04-05 12:00:59 +0200

    Adding finale progress bar

--- src/components/layouts/ProgressBar.tsx
+++ src/components/layouts/ProgressBar.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 import styles from './ProgressBar.module.scss';
 
-const ProgressBar = ({ percentage }: { percentage: number }) => {
-  const test = 'test';
-
-  return (
-    <div className={styles.container}>
-      ProgressBar
+const ProgressBar = ({ percentage }: { percentage: number }) => (
+  <div className={styles.container}>
+    <div className={styles.progressBarContainer}>
+      <div className={styles.progressBar}>
+        <div className={styles.progress} style={{ height: `${percentage * 100}%` }} />
+      </div>
+      <h2>{percentage * 100}%</h2>
     </div>
-  );
-};
+  </div>
+);
 
 export default ProgressBar;

5bca026a6bc7ecebc883e3671b304b1a66d224c1
Pmmarteau <pm.marteau@gmail.com>
2023-04-10 15:27:00 +0200

    Fix barrel imports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,2 @@
 export { default as MainLayout } from './layouts/MainLayout';
-export { default as Loader } from './ui/Loader';
 export { default as DescriptiveCard } from './rules/DescriptiveCard';

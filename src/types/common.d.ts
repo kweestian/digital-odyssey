ac7c98ebf09f7dd0c6f56bf7776e0ada32872395
Pmmarteau <pm.marteau@gmail.com>
2023-04-10 11:17:36 +0200

    Fix css layout for cards, flex -> grid

--- src/types/common.d.ts
+++
@@ -1,5 +0,0 @@
-import { NextPage } from 'next';
-
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: React.ReactElement) => React.ReactNode;
-};
--- src/types/env.d.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-namespace NodeJS {
-  interface ProcessEnv {
-    NEXT_PUBLIC_EXAMPLE_API_KEY: string;
-  }
-}

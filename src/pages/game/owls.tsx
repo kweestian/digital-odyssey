5d9f13f89e795b4efffe5227ea2654dde943ef46
Pmmarteau <pm.marteau@gmail.com>
2023-04-11 14:15:33 +0200

    Quick color change owl page

--- src/pages/game/owls.tsx
+++ src/pages/game/owls.tsx
@@ -4,19 +4,22 @@ import Image from 'next/image';
 import styles from '../../styles/Owls.module.scss';
 
 import * as BasicOwl from '../../../public/static/image/owls/basic-owl.svg';
+import * as BlueOwl from '../../../public/static/image/owls/blue-owl.svg';
+import * as PinkOwl from '../../../public/static/image/owls/pink-owl.svg';
+import * as GoldOwl from '../../../public/static/image/owls/gold-owl.svg';
 
 type Props = {};
 
 const Owls: NextPage<Props> = () => (
   <div className={styles.container}>
     <div className={styles.owlsList}>
-      <Image src={BasicOwl} alt="Basic Owl" />
-      <Image src={BasicOwl} alt="Basic Owl" />
+      <Image src={BlueOwl} alt="Basic Owl" />
+      <Image src={PinkOwl} alt="Basic Owl" />
       <Image src={BasicOwl} alt="Basic Owl" />
       <Image src={BasicOwl} alt="Basic Owl" />
       <Image src={BasicOwl} alt="Basic Owl" />
     </div>
-    <Image src={BasicOwl} alt="Basic Owl" />
+    <Image src={GoldOwl} alt="Basic Owl" />
   </div>
 );
 

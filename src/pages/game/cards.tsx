5c8f394c25ca133629d26f413705046375a2d2f4
Pmmarteau <pm.marteau@gmail.com>
2023-04-11 11:20:07 +0200

    Adding game cards

--- src/pages/game/cards.tsx
+++ src/pages/game/cards.tsx
@@ -1,13 +1,24 @@
 import { NextPage } from 'next';
 import useTranslation from 'next-translate/useTranslation';
 
+import GameCard from '../../components/GameCard';
+import { gameCards } from '../../data/cards';
+
+import styles from '../../styles/Cards.module.scss';
+
 type Props = {};
 
 const Cards: NextPage<Props> = () => {
   const { t } = useTranslation('common');
   const title = t('menuTitles.cards');
 
-  return <div>{title}</div>;
+  return (
+    <div className={styles.container}>
+      {gameCards.map((gameCard) => (
+        <GameCard title={gameCard.title} isActive={gameCard.isActive} cardUrl={gameCard.cardSvg} key={gameCard.title} />
+      ))}
+    </div>
+  );
 };
 
 export default Cards;

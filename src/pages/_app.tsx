081db598d2b097bd8bbe22a71df1cccda852c305
Pmmarteau <pm.marteau@gmail.com>
2023-04-12 17:26:58 +0200

    Try fix 500

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import type { AppProps } from 'next/app';
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
 
 import '@/styles/globals.scss';
 
@@ -9,24 +7,20 @@ import { Layout } from '../components';
 import { NextPageWithLayout } from '../types/common';
 import ErrorBoundary from './ErrorBoundary';
 
-type AppPropsWithLayout = AppProps<{ initialSession: Session }> & {
+type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
   locale?: string;
 };
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
-
-  const { initialSession, ...props } = pageProps;
+  const { ...props } = pageProps;
 
   return (
     <ErrorBoundary>
-      <SessionContextProvider supabaseClient={supabaseClient} initialSession={initialSession}>
-        <Layout>
-          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-          <Component {...props} />
-        </Layout>
-      </SessionContextProvider>
+      <Layout>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...props} />
+      </Layout>
     </ErrorBoundary>
   );
 }

74ca9a71de35f8ab60ea946dc2f53ba63023c283
Pmmarteau <pm.marteau@gmail.com>
2023-04-21 11:25:20 +0200

    Changing map from leaflet to svg pan zoom

--- src/pages/game/map.tsx
+++ src/pages/game/map.tsx
@@ -1,8 +1,37 @@
 import { NextPage } from 'next';
-import dynamic from 'next/dynamic';
-import { MapContainer } from 'react-leaflet';
+import Image from 'next/image';
+import { TransformWrapper, TransformComponent, ReactZoomPanPinchRef } from 'react-zoom-pan-pinch';
 
-const CustomMap = dynamic(() => import('@/components/games/map'), { ssr: false });
-const Map: NextPage = () => <CustomMap />;
+import styles from '@/styles/map.module.scss';
+
+import * as MapImage from '@/image/map/full-map.png';
+import * as CreativityCoast from '@/image/map/creativity-coast.svg';
+import { useRef } from 'react';
+
+const Map: NextPage = () => {
+  const transformComponentRef = useRef<ReactZoomPanPinchRef | null>(null);
+
+  const zoomToImage = () => {
+    if (transformComponentRef.current) {
+      const { zoomToElement } = transformComponentRef.current;
+      zoomToElement('creativityCoast');
+    }
+  };
+
+  return (
+    <div className={styles.mapContainer}>
+      <TransformWrapper ref={transformComponentRef}>
+        <TransformComponent>
+          <Image src={MapImage} alt="" className={styles.mapImageContainer} />
+          <div className={styles.svgRegions}>
+            <button onClick={zoomToImage} type="button" className={styles.creativityCoastContainer}>
+              <Image src={CreativityCoast} alt="" id="creativityCoast" className={styles.creativityCoast} />
+            </button>
+          </div>
+        </TransformComponent>
+      </TransformWrapper>
+    </div>
+  );
+};
 
 export default Map;

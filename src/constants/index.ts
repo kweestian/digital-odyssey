663aae2220662bbc05c2e262729468eb7309fe7e
Cristian <chris@enverselabs.com>
2023-05-09 16:32:35 +0200

    add @aldenteparis.com

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,1 @@
-export const ALLOWED_DOMAINS = ['@kering.com', '@aldente.com', '@enverselabs.com', '@heretique.fr'];
+export const ALLOWED_DOMAINS = ['@kering.com', '@aldenteparis.com', '@enverselabs.com', '@heretique.fr'];

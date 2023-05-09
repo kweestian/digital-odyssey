8ffeff9b896823f72e59ae262ce5bfbc5f9f5fe7
Cristian <chris@enverselabs.com>
2023-05-09 23:40:55 +0200

    use only english; set origin in redirect url

--- src/pages/auth/reset-password.tsx
+++ src/pages/auth/reset-password.tsx
@@ -17,7 +17,7 @@ const Login: NextPageWithLayout = () => {
 
       try {
         const { error: updateError, data } = await supabase.auth.resetPasswordForEmail(email, {
-          redirectTo: 'http://localhost:3000/auth/update-password',
+          redirectTo: `${window && window.location.origin}/auth/update-password`,
         });
 
         if (updateError) {

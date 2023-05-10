8f0e1025893f9150fc2bd3d14d715caf3a6e2d5f
Pmmarteau <pm.marteau@gmail.com>
2023-05-10 10:36:10 +0200

    Card ranked by regions

--- src/pages/game/cards/index.tsx
+++ src/pages/game/cards/index.tsx
@@ -11,17 +11,24 @@ type Props = {};
 const Cards: NextPage<Props> = () => {
   const { data } = useMapData();
   const allExperiences = data.map(({ experiences }) => experiences).flat();
+
+  console.log(data, allExperiences);
+
   return (
     <GameLayout>
       <div className={styles.container}>
-        {allExperiences.map(({ isCompleted, key, keyLearning }) => (
-          <RewardCard
-            addtitionalResrouces={keyLearning.additionalRessources}
-            content={keyLearning.text}
-            isActive={isCompleted}
-            cardUrl={`/static/image/cards/${key}.webp`}
-            key={key}
-          />
+        {data.map(({ regionKey, experiences }) => (
+          <div key={regionKey} className={styles.regionContainer}>
+            {experiences.map(({ isCompleted, key, keyLearning }) => (
+              <RewardCard
+                addtitionalResrouces={keyLearning.additionalRessources}
+                content={keyLearning.text}
+                isActive={isCompleted}
+                cardUrl={`/static/image/cards/${key}.webp`}
+                key={key}
+              />
+            ))}
+          </div>
         ))}
       </div>
     </GameLayout>

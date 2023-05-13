c42bff06cf39381d00860ba89d856bcb1a9e81cd
Pmmarteau <pm.marteau@gmail.com>
2023-05-13 16:04:24 +0200

    Fix poppin corner + close button position

--- src/components/common/PopupVIdeo/PopupVideo.tsx
+++ src/components/common/PopupVIdeo/PopupVideo.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import ReactPlayer from 'react-player';
 
 import styles from './PopupVideo.module.scss';
 
@@ -12,25 +11,23 @@ const PopupVideo = ({
   videoUrl: string;
   onClick: () => void;
   closeIcon: typeof import('*.svg');
-}) => {
-  const [hasWindow, setHasWindow] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setHasWindow(true);
-    }
-  }, []);
-
-  return (
-    <div className={styles.container}>
-      <div className={styles.iframeContainer}>
-        <button onClick={onClick} className={styles.closeButton} type="button">
-          <Image width={30} height={30} src={closeIcon} alt="Close Popup Button" />
-        </button>
-        {hasWindow && <ReactPlayer url={videoUrl} width="100%" height="100%" controls />}
-      </div>
+}) => (
+  <div className={styles.container}>
+    <div className={styles.iframeContainer}>
+      <button onClick={onClick} className={styles.closeButton} type="button">
+        <Image width={30} height={30} src={closeIcon} alt="Close Popup Button" />
+      </button>
+      <iframe
+        width="100%"
+        height="100%"
+        src={videoUrl}
+        title="Sustainability Progress Report 2020-2023 - KERING"
+        allow="accelerometer; clipboard-write; encrypted-media;
+          gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
     </div>
-  );
-};
+  </div>
+);
 
 export default PopupVideo;

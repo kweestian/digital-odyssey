b54da29335cf3f667274f03837b5c68145822dc3
Cristian <chris@enverselabs.com>
2023-05-15 15:06:15 +0200

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,13 @@ import nextJest from 'next/jest.js';
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^.+\\.(css|scss)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 });
 
 // Add any custom config to be passed to Jest
@@ -12,12 +19,25 @@ const createJestConfig = nextJest({
 const config = {
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^.+\\.(css|scss)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 
   testEnvironment: 'jest-environment-jsdom',
-  collectCoverageFrom: ['src/**/*.ts*', '!**/node_modules/**'],
-  coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
-  testMatch: ['**/*.test.ts', '**/*.test.tsx'],
-  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
+  coverageReporters: ['text', 'lcov'],
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/coverage',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: '<rootDir>/coverage',
+        outputName: 'junit.xml',
+      },
+    ],
+  ],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async

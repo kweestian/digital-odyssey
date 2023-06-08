e30335a4248b85927b8e620112582070b5d10588
Pmmarteau <pm.marteau@gmail.com>
2023-06-08 15:59:28 +0200

    Try fix pipeline

--- src/pages/game/map/timeless-tundra/index.tsx
+++ src/pages/game/map/timeless-tundra/index.tsx
@@ -4,57 +4,53 @@ import Image from 'next/image';
 
 import { GameLayout } from '@/components';
 
-import * as Certificate from '@/image/map/timeless-tundra/certificate.svg';
-import * as GoldenOwl from '@/image/rules/gold-owl-rules.gif';
+import Certificate from '@/image/map/timeless-tundra/certificate.svg';
+import GoldenOwl from '@/image/rules/gold-owl-rules.gif';
 
 import classNames from 'classnames';
 import styles from './timelessTundra.module.scss';
 
-const TimelessTundraPage: NextPage = () => {
-  const test = 'test';
-
-  return (
-    <GameLayout>
-      <div className={styles.container}>
-        <div className={styles.title}>
-          <Image
-            className={classNames(styles.goldenOwl, styles.firstOwl)}
-            src={GoldenOwl}
-            alt="Golden owl decoration 1"
-          />
-          <Image
-            className={classNames(styles.goldenOwl, styles.secondOwl)}
-            src={GoldenOwl}
-            alt="Golden owl decoration 2"
-          />
-          <Image
-            className={classNames(styles.goldenOwl, styles.thirdOwl)}
-            src={GoldenOwl}
-            alt="Golden owl decoration 3"
-          />
-          <Image
-            className={classNames(styles.goldenOwl, styles.fourthOwl)}
-            src={GoldenOwl}
-            alt="Golden owl decoration 4"
-          />
-          <Image
-            className={classNames(styles.goldenOwl, styles.fifthOwl)}
-            src={GoldenOwl}
-            alt="Golden owl decoration 5"
-          />
-          <p>CONGRATULATIONS</p>
-        </div>
-        <p className={styles.description}>
-          Congratulations, you're now part of the Digital Odyssey community of finishers. Download your <br />{' '}
-          certificate and post-it on our internal network Workplace
-        </p>
-        <Image className={styles.certificateImage} src={Certificate} alt="Certificate" />
-        <button className={styles.downloadButton} type="button">
-          DOWNLOAD YOUR CERTIFICATE
-        </button>
+const TimelessTundraPage: NextPage = () => (
+  <GameLayout>
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <Image
+          className={classNames(styles.goldenOwl, styles.firstOwl)}
+          src={GoldenOwl}
+          alt="Golden owl decoration 1"
+        />
+        <Image
+          className={classNames(styles.goldenOwl, styles.secondOwl)}
+          src={GoldenOwl}
+          alt="Golden owl decoration 2"
+        />
+        <Image
+          className={classNames(styles.goldenOwl, styles.thirdOwl)}
+          src={GoldenOwl}
+          alt="Golden owl decoration 3"
+        />
+        <Image
+          className={classNames(styles.goldenOwl, styles.fourthOwl)}
+          src={GoldenOwl}
+          alt="Golden owl decoration 4"
+        />
+        <Image
+          className={classNames(styles.goldenOwl, styles.fifthOwl)}
+          src={GoldenOwl}
+          alt="Golden owl decoration 5"
+        />
+        <p>CONGRATULATIONS</p>
       </div>
-    </GameLayout>
-  );
-};
+      <p className={styles.description}>
+        Congratulations, you're now part of the Digital Odyssey community of finishers. Download your <br /> certificate
+        and post-it on our internal network Workplace
+      </p>
+      <Image className={styles.certificateImage} src={Certificate} alt="Certificate" />
+      <button className={styles.downloadButton} type="button">
+        DOWNLOAD YOUR CERTIFICATE
+      </button>
+    </div>
+  </GameLayout>
+);
 
 export default TimelessTundraPage;

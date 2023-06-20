96218ff1435d68ff4c70182e9cc31a6ff10df270
Cristian <chris@enverselabs.com>
2023-06-20 13:13:13 +0200

    add new cards

--- src/pages/auth/login/email.tsx
+++ src/pages/auth/login/email.tsx
@@ -1,8 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
 import Link from 'next/link';
-import { useSession, useSessionContext, useSupabaseClient, useUser } from '@supabase/auth-helpers-react';
+import { useSessionContext, useSupabaseClient, useUser } from '@supabase/auth-helpers-react';
 import { useRouter } from 'next/router';
-import { useOnEnterCallback } from '@/hooks';
 
 import { DefaultLayout, Button, AuthInput } from '@/components';
 import { NextPageWithLayout } from '@/types/common';
@@ -73,29 +72,45 @@ const Login: NextPageWithLayout = () => {
     [setLoading, setError, setSuccess, push, supabase.auth],
   );
 
-  useOnEnterCallback(() => console.log(email, password));
-
   return (
     <DefaultLayout>
-      <div className={styles.form__group}>
-        <Image className={styles.logoImage} src={KeringImaginationLab} alt="Kering Imagination Lab Logo" />
-        <h4 className={styles.signInTitle}>SIGN IN USING YOUR EMAIL AND PASSWORD</h4>
+      <div className={styles.form__container}>
+        <div className={styles.form__group}>
+          <Image className={styles.logoImage} src={KeringImaginationLab} alt="Kering Imagination Lab Logo" />
+          <h4 className={styles.signInTitle}>SIGN IN USING YOUR EMAIL AND PASSWORD</h4>
 
-        <div className={styles.form__inputs}>
-          <div className={styles.form__emailInput}>
-            <AuthInput name="email" label="Email" type="input" onChange={(val) => setEmail(val)} />
-          </div>
-          <div className={styles.form__emailInput}>
-            <AuthInput name="password" label="Password" type="password" onChange={(val) => setPassword(val)} />
-            <div className={styles.message__container}>
-              {error && <span className={styles.errorSignIn}>{error}</span>}
-              {success && <span className={styles.successSignIn}>{success}</span>}
+          <div className={styles.form__inputs}>
+            <div className={styles.form__emailInput}>
+              <AuthInput name="email" label="Email" type="input" onChange={(val) => setEmail(val)} />
             </div>
+            <div className={styles.form__emailInput}>
+              <AuthInput name="password" label="Password" type="password" onChange={(val) => setPassword(val)} />
+              <div className={styles.message__container}>
+                {error && <span className={styles.errorSignIn}>{error}</span>}
+                {success && <span className={styles.successSignIn}>{success}</span>}
+              </div>
+            </div>
+            <Button text="Submit" skin="submit" loading={loading} onClick={() => signInWithEmail(email, password)} />
+            <Link href="/auth/login">Use magic link instead</Link>
+            <Link href="/auth/reset-password">Forgot password ?</Link>
           </div>
-
-          <Button text="Submit" skin="submit" loading={loading} onClick={() => signInWithEmail(email, password)} />
-          <Link href="/auth/login">Use magic link instead</Link>
-          <Link href="/auth/reset-password">Forgot password ?</Link>
+        </div>
+        <div className={styles.legal__paragraph}>
+          In the context of this serious game, Kering S.A. (acting as controller) will process personal data about you,
+          in particular screenshots you provide to unlock the various stages of the game. Your personal data will be
+          processed for the following purposes only: (i) enabling you to login and participate in the serious game; (ii)
+          identifying the winner(s) of the game and granting a reward; (iii) enabling you to post your completion
+          certificate on internal social media (provided you proactively decide to do so); (iv) managing troubleshooting
+          and maintenance; (v) following up participation in the game; (vi) ensuring compliance with legal and
+          regulatory obligations; and (vii) managing potential or actual litigation.
+          <div style={{ marginTop: 10 }} /> Please note that you benefit from certain rights, subject to local privacy
+          laws, such as the right to request access to, rectification and erasure of your personal data, or to restrict
+          or object to processing, as well as the right to data portability.
+          <div style={{ marginTop: 10 }} /> If you want to know more about the processing of your personal data and your
+          rights, please read our Privacy Policy available{' '}
+          <a href="https://www.keringfoundation.org/en/privacy-policy/" target="_blank" rel="noreferrer">
+            here.
+          </a>
         </div>
       </div>
     </DefaultLayout>
--- src/pages/auth/reset-password.tsx
+++ src/pages/auth/reset-password.tsx
@@ -1,8 +1,10 @@
-import { Button, DefaultLayout, Form, Input } from '@/components';
+import { AuthInput, Button, DefaultLayout, Form } from '@/components';
 import { useOnEnterCallback } from '@/hooks';
 import { NextPageWithLayout } from '@/types/common';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
+
+import styles from './login/Login.module.scss';
 
 const Login: NextPageWithLayout = () => {
   const supabase = useSupabaseClient();
@@ -43,10 +45,31 @@ const Login: NextPageWithLayout = () => {
 
   return (
     <DefaultLayout>
-      <Form title="Enter the email you signed up with" error={error} success={success}>
-        <Input name="email" label="Email" type="email" onChange={(val) => setEmail(val)} />
-        <Button text="Submit" loading={loading} onClick={() => sendPasswordLink()} />
-      </Form>
+      <div className={styles.form__container}>
+        <Form title="Enter the email you signed up with" error={error} success={success}>
+          <AuthInput name="email" label="Email" type="email" onChange={(val) => setEmail(val)} />
+          <div className={styles.submitButtonRow}>
+            <Button text="Submit" skin="submit" loading={loading} onClick={() => sendPasswordLink()} />
+          </div>
+        </Form>
+        <div className={styles.legal__paragraph}>
+          In the context of this serious game, Kering S.A. (acting as controller) will process personal data about you,
+          in particular screenshots you provide to unlock the various stages of the game. Your personal data will be
+          processed for the following purposes only: (i) enabling you to login and participate in the serious game; (ii)
+          identifying the winner(s) of the game and granting a reward; (iii) enabling you to post your completion
+          certificate on internal social media (provided you proactively decide to do so); (iv) managing troubleshooting
+          and maintenance; (v) following up participation in the game; (vi) ensuring compliance with legal and
+          regulatory obligations; and (vii) managing potential or actual litigation.
+          <div style={{ marginTop: 10 }} /> Please note that you benefit from certain rights, subject to local privacy
+          laws, such as the right to request access to, rectification and erasure of your personal data, or to restrict
+          or object to processing, as well as the right to data portability.
+          <div style={{ marginTop: 10 }} /> If you want to know more about the processing of your personal data and your
+          rights, please read our Privacy Policy available{' '}
+          <a href="https://www.keringfoundation.org/en/privacy-policy/" target="_blank" rel="noreferrer">
+            here.
+          </a>
+        </div>
+      </div>
     </DefaultLayout>
   );
 };

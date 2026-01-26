1bf7036c4f21f20c6bb55c5caa32f3bb9c923523
Christian Hamelin <chris@enverselabs.com>
2026-01-26 20:31:37 +0100

    remove allowed domains

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-import { ALLOWED_DOMAINS } from '@/constants';
+// import { ALLOWED_DOMAINS } from '@/constants';
 
 export async function middleware(req: NextRequest) {
   // We need to create a response and hand it to the supabase client to be able to modify the response headers.
@@ -18,11 +18,11 @@ export async function middleware(req: NextRequest) {
     } = await supabase.auth.getSession();
 
     // Check auth condition
-    if (ALLOWED_DOMAINS.some((emailPattern) => session?.user.email?.endsWith(emailPattern))) {
-      // Authentication successful, forward request to protected route.
+    // if (ALLOWED_DOMAINS.some((emailPattern) => session?.user.email?.endsWith(emailPattern))) {
+    //   // Authentication successful, forward request to protected route.
 
-      return res;
-    }
+    //   return res;
+    // }
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error(e);
